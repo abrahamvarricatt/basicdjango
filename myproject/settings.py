119a7c7ccfa1ac7f2d6a1dbe7cb792f7683d26c8
AbrahamV <abraham.varricatt@googlemail.com>
2016-05-22 15:36:48 +0530

    static and settings update

--- myproject/settings.py
+++ myproject/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+# WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 # Database
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/srv/sources/STATIC_FILES'
 
