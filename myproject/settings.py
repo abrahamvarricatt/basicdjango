ac6a6b9c2623a705fbcc856ff88df2c969904b66
AbrahamV <abraham.varricatt@googlemail.com>
2016-05-22 15:46:35 +0530

    update static path

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/srv/sources/STATIC_FILES'
+STATIC_ROOT = '/srv/STATIC_FILES'
 
